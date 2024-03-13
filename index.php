5d41d7e342fa21152c92befb2728abca5a561c6a
TroHovasapian <121320107+TroHovasapian@users.noreply.github.com>
2024-03-12 20:12:12 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Foreign Flavors</title>
+    <title>GoodEats</title>
     <!-- <link rel="stylesheet" href="assets/css/styles.css"> -->
     <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
-    <br/>
     <header>
-        <h1>Welcome to Foreign Flavors</h1>
+        <h1>GoodEats</h1>
     </header>
     <br/>
     <nav class="navbar">
         <ul>
             <li><a href="pages/login.php">Login</a></li>
-            <li><a href="pages/register.php">Make An Account</a></li>
+            <li><a href="pages/register.php">Register</a></li>
         </ul>
     </nav>
 </body>
